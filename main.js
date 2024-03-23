ae3522fd206fbab034fdbe8812aa23b60ab8f7a6
Liza Dorozhynska <liza.hazard20@gmail.com>
2024-03-23 10:56:02 +0000

    fix: fix json

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-import { words }  from './words.js' 
+const words = JSON.parse(`[
+    {
+        "word": "secret",
+        "hint": "secret is a word"
+    },
+    {
+        "word": "pirat",
+        "hint": "pirat is a word"
+    },
+    {
+        "word": "sekira",
+        "hint": "sekira is a word"
+    },
+    {
+        "word": "machete",
+        "hint": "machete is a word"
+    },
+    {
+        "word": "shark",
+        "hint": "shark is a word"
+    },
+    {
+        "word": "ship",
+        "hint": "ship is a word"
+    }
+]`)
 const gameBlock = document.querySelector('#game')
 const gameStartBtn = document.createElement('button')
 let secretWord = 'secret'
